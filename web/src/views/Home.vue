875e4015225e0f617b71707dc102985a3aa4f6f9
Deming Wang <wangdeming@gmail.com>
2021-09-27 14:59:26 -0500

    implement axios data with UI display using ref/reactive

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -3,20 +3,38 @@
     <a-button type="primary">Primary Button</a-button>
     <img alt="Vue logo" src="../assets/logo.png">
     <!--<HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>-->
+    <pre>
+      {{ebooks}}
+      {{ebooks2}}
+    </pre>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, onMounted, ref, reactive, toRef } from 'vue';
 import axios  from "axios";
 export default defineComponent({
   name: 'Home',
   setup(){
     console.log("setup");
-    axios.get("http://localhost:8880/ebook/list?name=Spring")
-        .then(function(response){
-          console.log(response);
-        });
+    const ebooks = ref();  //responsible data of UI element
+    const ebooks1 = reactive({books:[]});
+
+    onMounted(()=>{
+      console.log("onMounted");
+      axios.get("http://localhost:8880/ebook/list?name=Spring")
+          .then(function(response){
+            const data = response.data;
+            ebooks.value = data.content;
+            ebooks1.books = data.content;
+            console.log(response);
+          });
+    });
+
+    return {
+      ebooks,
+      ebooks2: toRef(ebooks1, "books")
+    };
   },
   components: {
   },
