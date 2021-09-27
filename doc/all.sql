c1a29454ccd613ca572405cc3d4ba040224c6f44
Deming Wang <wangdeming@gmail.com>
2021-09-27 09:37:57 -0500

    generate ebook mybatis classes

--- doc/all.sql
+++ doc/all.sql
@@ -15,3 +15,23 @@ create table `demo` (
                         primary key (`id`)
 ) engine=innodb default charset=utf8mb4 comment='test';
 insert into `demo` (id, name) values(1, 'demouser');
+
+#ebook
+drop table if exists `ebook`;
+create table `ebook`(
+        `id` bigint not null comment 'id',
+        `name` varchar(50) comment 'name',
+        `category1_id` bigint comment 'category1',
+        `category2_id` bigint comment 'category2',
+        `description` varchar(200) comment 'description',
+        `cover` varchar(200) comment 'cover',
+        `doc_count` int comment 'doc count',
+        `view_count` int comment 'view count',
+        `vote_count` int comment 'vote count',
+        primary key (`id`)
+)engine=innodb default charset=utf8mb4 comment='ebook';
+
+insert into `ebook`(id, name, description) values(1, 'Spring Boot Introduction', 'Spring Boot Introduction Book Description');
+insert into `ebook`(id, name, description) values(2, 'Python Introduction', 'Python Introduction Book Description');
+insert into `ebook`(id, name, description) values(3, 'Vue Introduction', 'Vue Introduction Book Description');
+insert into `ebook`(id, name, description) values(4, 'Oracle Introduction', 'Oracle Introduction Book Description');
