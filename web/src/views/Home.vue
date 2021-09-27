9d3561b9dd36744ebf29ea0a5ad5fb7fc5290a6d
Deming Wang <wangdeming@gmail.com>
2021-09-27 16:43:15 -0500

    ebook layout 3 columns adjust

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,35 +1,21 @@
 <template>
   <a-layout>
     <a-layout-content>
-      <a-list item-layout="vertical" size="large" :pagination="pagination" :data-source="listData">
-      <template #footer>
-        <div>
-          <b>ant design vue</b>
-          footer part
-        </div>
-      </template>
+      <a-list item-layout="vertical" size="large" :grid="{gutter: 20, column:3}"  :data-source="ebooks">
       <template #renderItem="{ item }">
-        <a-list-item key="item.title">
+        <a-list-item key="item.name">
           <template #actions>
           <span v-for="{ type, text } in actions" :key="type">
             <component v-bind:is="type" style="margin-right: 8px" />
             {{ text }}
           </span>
           </template>
-          <template #extra>
-            <img
-                width="272"
-                alt="logo"
-                src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png"
-            />
-          </template>
           <a-list-item-meta :description="item.description">
             <template #title>
-              <a :href="item.href">{{ item.title }}</a>
+              <a :href="item.href">{{ item.name }}</a>
             </template>
-            <template #avatar><a-avatar :src="item.avatar" /></template>
+            <template #avatar><a-avatar :src="item.cover" /></template>
           </a-list-item-meta>
-          {{ item.content }}
         </a-list-item>
       </template>
     </a-list>
@@ -39,7 +25,7 @@
 
 <script lang="ts">
 import { StarOutlined, LikeOutlined, MessageOutlined } from '@ant-design/icons-vue';
-import { defineComponent, onMounted, ref, reactive } from 'vue';
+import { defineComponent, onMounted, ref } from 'vue';
 
 const listData: Record<string, string>[] = [];
 for (let i = 0; i < 23; i++) {
@@ -65,7 +51,6 @@ export default defineComponent({
   setup(){
     console.log("setup");
     const ebooks = ref();  //responsible data of UI element
-    const ebooks1 = reactive({books:[]});
 
     onMounted(()=>{
       console.log("onMounted");
@@ -73,7 +58,6 @@ export default defineComponent({
           .then(function(response){
             const data = response.data;
             ebooks.value = data.content;
-            ebooks1.books = data.content;
             console.log(response);
           });
     });
@@ -91,6 +75,7 @@ export default defineComponent({
     ];
 
     return {
+      ebooks,
       listData,
       pagination,
       actions,
