884983426660f7fcd9171a12372617347dd30258
Deming Wang <wangdeming@gmail.com>
2021-09-26 21:50:14 -0500

    second upate

--- src/main/java/com/deming/blog/BlogApplication.java
+++ src/main/java/com/deming/blog/BlogApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BlogApplication {
-
+	//default main application
 	public static void main(String[] args) {
 		SpringApplication.run(BlogApplication.class, args);
 	}
