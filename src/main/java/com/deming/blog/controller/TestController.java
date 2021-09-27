0e2bbbc50e5e262139f4061510a8ce49788eed36
Deming Wang <wangdeming@gmail.com>
2021-09-26 22:50:03 -0500

    @Value("${test.hello:Test}"), connected to application.properties

--- src/main/java/com/deming/blog/controller/TestController.java
+++ src/main/java/com/deming/blog/controller/TestController.java
@@ -1,5 +1,8 @@
 package com.deming.blog.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,14 +10,21 @@ import org.springframework.web.bind.annotation.RestController;
 //Controller for html page, RestController for txt data only.
 public class TestController {
 
+    @Value("${test.hello:Test}")
+    private String testHello;
+
     /**
      * GET, POST, PUT, DELTE
      * /user?id=1
      * /user/1
-     * @return txt message
      */
-    @RequestMapping("/hello")
+    @GetMapping("/hello")
     public String hello(){
-        return "hello world!";
+        return "hello world!" + testHello;
+    }
+
+    @PostMapping("/hello/post")
+    public String helloPost(String name){
+        return "hello world! Post," + name;
     }
 }
