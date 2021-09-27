45c558afef84c227c603078ef2809a822b67fd5a
Deming Wang <wangdeming@gmail.com>
2021-09-27 05:46:54 -0500

    add all.sql and mybatis support

--- src/main/java/com/deming/blog/BlogApplication.java
+++ src/main/java/com/deming/blog/BlogApplication.java
@@ -4,16 +4,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.core.env.Environment;
-
-import javax.swing.*;
 
 //@ComponentScan({"com.xxx", "com.yy...."})  add packages to be scanned
 @SpringBootApplication
 public class BlogApplication {
 
-	private static Logger LOG = LoggerFactory.getLogger(BlogApplication.class);
+	private static final Logger LOG = LoggerFactory.getLogger(BlogApplication.class);
 
 	public static void main(String[] args) {
 		//SpringApplication.run(BlogApplication.class, args);
