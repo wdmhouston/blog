de7830791aea00d4512a4039023e8411a94c5582
Deming Wang <wangdeming@gmail.com>
2021-09-27 20:35:30 -0500

    add aop and disable logfilter/loginterceptor

--- src/main/java/com/deming/blog/interceptor/LogInterceptor.java
+++ src/main/java/com/deming/blog/interceptor/LogInterceptor.java
@@ -1,37 +1,37 @@
- package com.deming.blog.interceptor;
-
- import org.slf4j.Logger;
- import org.slf4j.LoggerFactory;
- import org.springframework.stereotype.Component;
- import org.springframework.web.servlet.HandlerInterceptor;
- import org.springframework.web.servlet.ModelAndView;
-
- import javax.servlet.http.HttpServletRequest;
- import javax.servlet.http.HttpServletResponse;
-
- /**
-  * 拦截器：Spring框架特有的，常用于登录校验，权限校验，请求日志打印 /login
-  */
- @Component
- public class LogInterceptor implements HandlerInterceptor {
-
-     private static final Logger LOG = LoggerFactory.getLogger(LogInterceptor.class);
-
-     @Override
-     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-         // 打印请求信息
-         LOG.info("------------- LogInterceptor Start -------------");
-         LOG.info("Request Address: {} {}", request.getRequestURL().toString(), request.getMethod());
-         LOG.info("Remote Address: {}", request.getRemoteAddr());
-
-         long startTime = System.currentTimeMillis();
-         request.setAttribute("requestStartTime", startTime);
-         return true;
-     }
-
-     @Override
-     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-         long startTime = (Long) request.getAttribute("requestStartTime");
-         LOG.info("------------- LogInterceptor End. Time：{} ms -------------", System.currentTimeMillis() - startTime);
-     }
- }
+// package com.deming.blog.interceptor;
+//
+// import org.slf4j.Logger;
+// import org.slf4j.LoggerFactory;
+// import org.springframework.stereotype.Component;
+// import org.springframework.web.servlet.HandlerInterceptor;
+// import org.springframework.web.servlet.ModelAndView;
+//
+// import javax.servlet.http.HttpServletRequest;
+// import javax.servlet.http.HttpServletResponse;
+//
+// /**
+//  * 拦截器：Spring框架特有的，常用于登录校验，权限校验，请求日志打印 /login
+//  */
+// @Component
+// public class LogInterceptor implements HandlerInterceptor {
+//
+//     private static final Logger LOG = LoggerFactory.getLogger(LogInterceptor.class);
+//
+//     @Override
+//     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+//         // 打印请求信息
+//         LOG.info("------------- LogInterceptor Start -------------");
+//         LOG.info("Request Address: {} {}", request.getRequestURL().toString(), request.getMethod());
+//         LOG.info("Remote Address: {}", request.getRemoteAddr());
+//
+//         long startTime = System.currentTimeMillis();
+//         request.setAttribute("requestStartTime", startTime);
+//         return true;
+//     }
+//
+//     @Override
+//     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+//         long startTime = (Long) request.getAttribute("requestStartTime");
+//         LOG.info("------------- LogInterceptor End. Time：{} ms -------------", System.currentTimeMillis() - startTime);
+//     }
+// }
